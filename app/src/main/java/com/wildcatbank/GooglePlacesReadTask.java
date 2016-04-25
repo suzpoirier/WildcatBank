a9f9ab6f3eee6bfcbe43ad2987db3755c86a7bb8
suzpoirier <suz.poirier@gmail.com>
2016-04-25 18:51:21 -0400

    Initial commit

---
+++ app/src/main/java/com/wildcatbank/GooglePlacesReadTask.java
@@ -0,0 +1,12 @@
+package com.wildcatbank;
+
+/**
+ * Created by SEPoirier on 4/22/2016.
+ */
+
+import android.util.Log;
+
+public class GooglePlacesReadTask  {
+
+
+}
---
+++ app/src/main/java/com/wildcatbank/MainActivity.java
@@ -0,0 +1,77 @@
+package com.wildcatbank;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.FragmentManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+
+public class MainActivity extends Activity implements AccountTabs.OnFragmentInteractionListener, ButtonSignIn.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+      //  setSupportActionBar(toolbar);
+
+     /*   FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onAccountTabFragmentInteraction(Uri uri) {
+
+    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+    public void launchDialog(View view){
+        FragmentManager fm = getFragmentManager();
+        ContactUs dialog = new ContactUs();
+        dialog.show(fm, "fragment_contact_us");
+    }
+    public void launchLocateActivity(View view){
+        Intent intent = new Intent(this, LocateActivity.class);
+        startActivity(intent);
+    }
+
+}
